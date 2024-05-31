859b04e737bb5dcf4935390e86372bd673b7c1ba
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-05-31 14:22:17 +0600

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// pages/index.js
+
+import axios from "axios";
+import Head from "next/head";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+export async function getStaticProps() {
+  // Fetch data from JSONPlaceholder API
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const posts = response.data;
 
-export default function Home() {
+  return {
+    props: { posts }, // Will be passed to the page component as props
+  };
+}
+
+function HomePage({ posts }) {
   return (
     <div>
-      <Link href="/posts">Posts</Link>
+      <Head>
+        <title>Blog Posts</title>
+        <meta
+          name="description"
+          content="A list of blog posts from JSONPlaceholder."
+        />
+      </Head>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              {post.title}
+            </Link>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default HomePage;
