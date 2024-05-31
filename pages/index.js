e5efd8f464f7d5ff024e80135d9d4d4e7540d937
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-05-31 14:01:33 +0600

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/posts">Posts</Link>
+    </div>
+  );
+}
